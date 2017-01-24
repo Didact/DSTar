4e423efb15c6a4ce8daa314cbd848b48fb85ea08
Dakota Smith <dsmith@appareo.com>
2017-01-23 23:00:33 -0700

    Initialize project

---
+++ DSTar/DSTar.h
@@ -0,0 +1,19 @@
+//
+//  DSTar.h
+//  DSTar
+//
+//  Created by dakota on 1/23/17.
+//  Copyright © 2017 Dakota Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DSTar.
+FOUNDATION_EXPORT double DSTarVersionNumber;
+
+//! Project version string for DSTar.
+FOUNDATION_EXPORT const unsigned char DSTarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSTar/PublicHeader.h>
+
+
